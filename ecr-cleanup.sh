b742a0e5a38df69fd168b4e73a444e0e56cc534a
Steven Nemetz <snemetz@wiser.com>
2017-10-20 16:09:36 -0700

    Commit to start testing in Jenkins

--- ecr-cleanup.sh
+++ ecr-cleanup.sh
@@ -4,7 +4,38 @@
 #
 # Clean all ECR repositories in AWS account
 #
+# Cleanup rules:
+#   YYYY-MM-DD*         older than x days
+#   x.y.z-d-sha         if newer x.y.z exists
+#   d                   If no other tags
+#   x.y.z-d-sha         No more than x per tag version
+#
+daysOld=5
+dateCutoff=$(date -d '5 days ago' +%Y-%m-%d)
+dateCutoffSec=$(date -d ${dateCutoff} +%s)
 for R in $(aws ecr describe-repositories | jq -r .repositories[].repositoryName); do
-  tags=$(aws ecr list-images --repository-name ${R} | jq -r .imageIds[].imagetTag)
+  echo "Cleaning ECR repository: ${R}"
+  tags=$(aws ecr list-images --repository-name ${R} | jq -r .imageIds[].imageTag | sort)
+  tagDates=$(echo "${tags}" | grep -E '^\d\d\d\d-\d\d-\d\d')
+  tagVersions=$(echo "${tags}" | grep -E '^\d+\.\d+\.\d+')
+  #echo "Tags: ${tags}"
+  echo "Tag Dates: ${tagDates}"
+  echo "Tag Versions: ${tagVersions}"
+  #for T in ${tags}; do
+  #  echo $T
+  #done
+  ### Cleanup date tags
+  toDelete=''
+  for date in ${tagDates}; do
+    # TODO: move to python for more portable data manipulation ?
+    dateBase="${date%%_*}"
+    dateSec=$(date -d ${dateBase} +%s)
+    if [ ${dateCutoffSec} -gt ${dateSec} ]; then
+      toDelete="${toDelete} ${date}"
+    fi
+  done
+  echo "Delete dates: ${toDelete}"
   #aws ecr batch-delete-image --repository-name ${R} --image-ids imageTag=a imageTag=b
+  ### Cleanup version tags
+  ### Cleanup single number tags
 done
